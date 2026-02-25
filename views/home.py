60a90894ae15a2ad3584ae5a687bfe46485d4753
Silja23 <haeslsil@students.zhaw.ch>
2026-02-25 11:12:37 +0100

    E-mail angepasst

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Silja Häsler (haeslsil@students.zhaw.ch)
+- Nadja Kuhn (kuhnnad1@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
